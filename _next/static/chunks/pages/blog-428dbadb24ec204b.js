fd56fedfe9f9e45a8a3fd31e9d9c30d072aee4a8
Muhammad Kamran <muhammadkamran8586@gmail.com>
2023-01-22 20:05:48 +0500

    first commit

---
+++ _next/static/chunks/pages/blog-428dbadb24ec204b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[195],{3986:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog",function(){return t(6531)}])},6531:function(n,u,t){"use strict";t.r(u);var _=t(5893);t(7294);let e=()=>(0,_.jsx)(_.Fragment,{});u.default=e}},function(n){n.O(0,[774,888,179],function(){return n(n.s=3986)}),_N_E=n.O()}]);
